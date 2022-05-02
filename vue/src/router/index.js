5b40b0154fb0e30ced6aa82d8a6b6a5783391dbb
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-03 00:14:17 +0200

    Created AuthLayout component. Moved common html from login view to authlayout. Made redirection from dashboard to login page if not authenticated. Made redirection form auth/* to dashboard if not authenticated.
    Login and register views now have correct content.

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,7 +3,11 @@ import DashboardView from '../views/DashboardView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import SurveysView from '../views/SurveysView.vue'
+
 import DefaultLayout from '../components/DefaultLayout.vue'
+import AuthLayout from '../components/AuthLayout.vue'
+
+import store from '@/store'
 
 const routes = [
   {
@@ -11,20 +15,22 @@ const routes = [
     redirect: '/dashboard',
     name: 'Dashboard',
     component: DefaultLayout,
+    meta: { requiresAuth: true },
     children: [
       { path: '/dashboard', name: 'Dashboard', component: DashboardView },
       { path: '/surveys', name: 'Surveys', component: SurveysView }
     ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: LoginView
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: RegisterView
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    meta: { isGuest: true },
+    component: AuthLayout,
+    children: [
+      { path: '/login', name: 'Login', component: LoginView },
+      { path: '/register', name: 'Register', component: RegisterView }
+    ]
   }
 ]
 
@@ -32,5 +38,16 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: 'Login' })
+  } else if (store.state.user.token && to.meta.isGuest) {
+    next({ name: 'Dashboard' })
+  } else {
+    next()
+  }
+}
+)
 
 export default router
