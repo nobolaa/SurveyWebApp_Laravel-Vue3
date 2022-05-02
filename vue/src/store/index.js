33ebfe42aab222d285821b1f8015db3803dbd1bc
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-02 20:52:39 +0200

    Installed vue3.js with vuex under folder vue/

---
+++ vue/src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    user: {
+      data: {
+        name: 'Nabil'
+      },
+      token: null
+    }
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
